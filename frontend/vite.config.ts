61dfc8efeb11259e55ecc71e217161774c4d0260
NotMyPersonalAccount <kevinzj710@gmail.com>
2026-09-09 00:01:05 -0700

    PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,46 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        screenshots: [
+          {
+            src: "/images/screenshot-wide.png",
+            sizes: "1920x924",
+            form_factor: "wide"
+          },
+        ],
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5173,
     proxy: {
